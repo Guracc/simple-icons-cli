3ee48a86e708516c58afc3b7ea3b88c4e8982422
Guracc <garaccioni.andrea@gmail.com>
2026-01-22 10:19:43 +0100

    Update Homebrew formula to v0.2.2

--- Formula/simple-icons.rb
+++ Formula/simple-icons.rb
@@ -3,8 +3,8 @@ class SimpleIcons < Formula
 
   desc "CLI for Simple Icons with icon format conversion"
   homepage "https://github.com/Guracc/simple-icons-cli"
-  url "https://github.com/Guracc/simple-icons-cli/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "ee87548490c8c307d9a7943c11fc1e224c63c0958092eaf704a77c6dc3f412d6"
+  url "https://github.com/Guracc/simple-icons-cli/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "17df9069973a8f04ae3784511fe1643974cb58566b37e49b900eebfc072cfd5b"
   license "MIT"
 
   depends_on "cairo"
